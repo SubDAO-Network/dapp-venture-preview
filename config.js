e5c18f5f804b194e91f6eeeedb892fb2e317b6c8
srdothunter <example@github.com>
2022-03-15 18:02:19 +0000

    Automatic commit

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 window.mainAddress = {
   DaoFactory: "0xCAa09e779408Db48C6ad303e8ac93bfD0d89F753",
   // for add or remove token
-  actionOptToken: "0xFC3E98bEe5de56118f1eEcA48DAD91889B11c239",
+  actionVaultOptToken: "0xFC3E98bEe5de56118f1eEcA48DAD91889B11c239",
   // for deposit or withdraw token
-  actionDwToken: "0x2021D9265B130B6B23F0f70Cd2CaFf98c710eeB1",
+  actionVaultDeposit: "0x2021D9265B130B6B23F0f70Cd2CaFf98c710eeB1",
   
   // for vote
-  actionNewVote: '0x8e2c198575ED200EC9B958F4784B8193757580bc',
-  actionVote: "0x77E54EFeEf02146C6e90661b676Be1D269a8a86F",
-  actionExecuteVote: '0xd95a33ecB11C78b1FF4D36073291b08B900Fe14A',
+  actionVoteNew: '0x8e2c198575ED200EC9B958F4784B8193757580bc',
+  actionVoteVote: "0x77E54EFeEf02146C6e90661b676Be1D269a8a86F",
+  actionVoteExecute: '0xd95a33ecB11C78b1FF4D36073291b08B900Fe14A',
   actionVaultWithdrawToken: '0x15c77f1EE3Fd6391ee8FCfdFf8C4930e57a149Ab',
   actionVaultExeWithdrawToken: '0x3A4cDe6D856cE7421b563FE50f54e6731196E18a',
   actionGrantMethodRegister: '0x0bBC94675908de91D805D71EAf589BD71a26863B',
@@ -19,9 +19,9 @@ window.mainAddress = {
   actionVoteTransferPeriodApply: '0xEd3D5709C7e572443Ec183645B447A3349f7bC04',
 
   // for org
-  actionOrg: "0x23bE5b7e4AfF9e6c281df499A3472ecDc48d829e",
-  ActionOrgMemberQuit: "0xa4152c13003b3f7f28D83c0831d4E8052FcA249D",
-  ActionOrgTransferOwner: "0x15D717f1Efc665CbFe3B901a8689A7DC63acd77E",
+  actionOrgMgr: "0x23bE5b7e4AfF9e6c281df499A3472ecDc48d829e",
+  actionOrgMemberQuit: "0xa4152c13003b3f7f28D83c0831d4E8052FcA249D",
+  actionOrgTransferOwner: "0x15D717f1Efc665CbFe3B901a8689A7DC63acd77E",
   actionOrgSetJoinMode: '0x038DCcCCd6FFa63F6a358A1a99Ba0a4bF6427F93',
   actionOrgApplyJoin: '0x1Db1301549b43837F10F0c530BD63CA9a95aD59D',
 
