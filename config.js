0b3deb43e0e61203da5035ba09b31d385aef08fc
srdothunter <example@github.com>
2022-03-15 13:09:19 +0000

    Automatic commit

--- config.js
+++ config.js
@@ -13,8 +13,10 @@ window.mainAddress = {
   actionVaultExeWithdrawToken: '0x3A4cDe6D856cE7421b563FE50f54e6731196E18a',
   actionGrantMethodRegister: '0x919ea6f0884726796933B4a372186E843e0b9315',
   actionGrantMethodApply: '0x3D8518293cdF8143dD464A5Fb538e48faF68993E',
-  actionVoteImpeachRegister: '0xa1380Ea1a3B67cfB4282C6c49E59e350e3851346',
-  actionVoteImpeachApply: '0xF879a9cCD111DCc5946b07F011c76D03dcF0F636',
+  actionVoteImpeachRegister: '0xd5ae94e4Ffd1e220A480Fe502005E67c808D63a0',
+  actionVoteImpeachApply: '0xF35374010110B7293F00a314071955Bf3F951e1d',
+  actionVoteTransferPeriodRegister: '0x5f48Dbc8259E6688ff376eCaE2282c44d853122a',
+  actionVoteTransferPeriodApply: '0xEd3D5709C7e572443Ec183645B447A3349f7bC04',
 
   // for org
   actionOrg: "0x23bE5b7e4AfF9e6c281df499A3472ecDc48d829e",
@@ -22,6 +24,17 @@ window.mainAddress = {
   ActionOrgTransferOwner: "0x15D717f1Efc665CbFe3B901a8689A7DC63acd77E",
   actionOrgSetJoinMode: '0x038DCcCCd6FFa63F6a358A1a99Ba0a4bF6427F93',
   actionOrgApplyJoin: '0x1Db1301549b43837F10F0c530BD63CA9a95aD59D',
+
+  // for venture
+  actionVenturesRaiseMoney: '0x2368d54b4dCc553ff2377641bBFDc1Fe762A5E09',
+  actionVenturesRaiseFail: '0x977F77541DCdeAf7b8D606Bd40b4299240Dea2a9',
+  actionVenturesOpenGPRaise: '0x95f608cc3E575F8677d4d3AC5E35765faA166B5f',
+  actionVenturesOpenLPRaise: '0x695C01E2eAeE4c418b5990a2bC9A33C731C07A02',
+  actionVenturesToRaiseFail: '0x131CE4Da3f9DcC249f571897f7B06608d746FDCB',
+  actionVenturesRaiseFailClaim: '0x97689aE0803afd488E1e6E7B6ac6648284607Af5',
+  actionVenturesToFreezingPeriod: '0xa375E87f4CCc0eaf086f907effB42CBf6D56A961',
+  actionVenturesClaimShare: '0xFB7E7701cb50137D5972EeC24f8585ec4E466c8c',
+  actionVaultUniswapV2SwapToken: '0xD51b6b3ae4ec43A141184e37c70d07B31E5f12Ce',
 
   tokenAddress: '0xbaDd22fA317b453f9343eA8A3971a0E0102899a2',
   tokenSymbol:'GOV',
